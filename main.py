e1a5b354a609b411774dfed63f31c39017091fc7
eden060398 <eden060398@gmail.com>
2020-06-01 16:56:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from models import *
 from config import *
 
 if __name__ == '__main__':
-    model = train_gan('./Break-dataset/QDMR/train.csv')
+    model = train_gan('/Break-dataset/QDMR/train.csv')
     model.save_internal(seq2seq_path='model.dat')
 
     eval_model([('model.dat', RobertaDecomposer, SEQ_LENGTH)],
-               './Break-dataset/QDMR/dev.csv',
+               '/Break-dataset/QDMR/dev.csv',
                orig_filenames=["orig.csv"],
                pred_filenames=["pred.csv"])
